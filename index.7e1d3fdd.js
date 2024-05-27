2020c51d076c7c28e1727ce0fadc2217ddfd2cb0
OleksChernikov <pjatochkin2013@gmail.com>
2024-05-27 16:18:10 +0300

    make build

---
+++ index.7e1d3fdd.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__bady--with-menu"):document.body.classList.remove("page__bady--with-menu")});
+//# sourceMappingURL=index.7e1d3fdd.js.map
